8dab1a558044ed4164ad2b3ce865ff4de8910613
Hasib98 <phoenixoffire1998@gmail.com>
2024-10-07 21:31:13 +0600

    title

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
 export default function App() {
-  return <div className="bg-red-500">TODO</div>;
+  return (
+    <div className="flex flex-col items-center space-y-7">
+      <Title />
+      <SatelliteCardBundle />
+    </div>
+  );
+}
+
+export function Title() {
+  return (
+    <h1 className="text-4xl  font-kavoon text-white mt-16">
+      Welcome to Space Info Scope
+    </h1>
+  );
+}
+export function SatelliteCardBundle() {
+  return (
+    <div className="flex space-x-4 ">
+      <SatelliteCard />
+      <SatelliteCard />
+      <SatelliteCard />
+      <SatelliteCard />
+      <SatelliteCard />
+    </div>
+  );
+}
+
+export function SatelliteCard() {
+  return <div className="bg-white p-4 rounded-lg w-14 text-center">card</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kavoon: ["Kavoon", "system-ui"],
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
