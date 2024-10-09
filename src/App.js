c66c8be4e83fe4d554032ec6d41fedd2aaa686b2
Hasib98 <phoenixoffire1998@gmail.com>
2024-10-09 21:56:05 +0600

    API INTEGRATION | NASA CALTECH SPACE OBSERVATORTY DATA

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
+// const encodedValue = encodeURIComponent(cardSatellite);
 import { useState, useEffect } from "react";
 
 // import "./assets/paper-plane.svg";
 export default function App() {
-  const [cardSatellite, setCardSatellite] = useState(
-    "Transiting Exoplanet Survey Satellite (TESS)"
-  );
-  const [planetTitle, setPlanetTitle] = useState("");
-
+  const [activeSatellite, setActiveSatellite] = useState("");
+  const [planetList, setPlanetList] = useState([]);
   useEffect(() => {
-    const fetchPlanets = async () => {
-      if (!cardSatellite) return;
-
-      const encodedValue = encodeURIComponent(cardSatellite);
-      const url = `https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=SELECT+pl_name+FROM+ps+WHERE+disc_facility='${encodedValue}'&format=json`;
-
+    const encodedValue = encodeURIComponent(activeSatellite);
+    // const url =
+    //   "https://corsproxy.io/?https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=SELECT+TOP+5+pl_name+FROM+ps+WHERE+disc_facility=%27Transiting%20Exoplanet%20Survey%20Satellite%20(TESS)%27&format=json";
+    const url = `https://corsproxy.io/?https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=SELECT+TOP+5+pl_name+FROM+ps+WHERE+disc_facility='${encodedValue}'&format=json`;
+    async function fecthData() {
       try {
-        const response = await fetch(url, { mode: "no-cors" });
-        const data = await response;
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching planets:", error);
-      }
-    };
+        const res = await fetch(url);
 
-    fetchPlanets();
-  }, [cardSatellite]);
+        if (!res.ok) {
+          console.log("no responspe");
+        }
+        const data = await res.json();
+        console.log(data);
+        setPlanetList(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fecthData();
+  }, [activeSatellite]);
 
   return (
     <div className="flex flex-col items-center space-y-7">
       <Title />
       <Search />
-      <SatelliteCardBundle setCardSatellite={setCardSatellite} />
-      <PlanetCard cardSatellite={cardSatellite} planetTitle={planetTitle} />
+      <SatelliteCardBundle setActiveSatellite={setActiveSatellite} />
+      <PlanetCard activeSatellite={activeSatellite} planetList={planetList} />
     </div>
   );
 }
@@ -44,7 +45,7 @@ export function Title() {
   );
 }
 
-export function Search() {
+function Search() {
   return (
     <div>
       <input type="text" />
@@ -52,7 +53,7 @@ export function Search() {
     </div>
   );
 }
-export function SatelliteCardBundle({ setCardSatellite }) {
+function SatelliteCardBundle({ setActiveSatellite }) {
   const satelliteArray = [
     "Transiting Exoplanet Survey Satellite (TESS)",
     "James Webb Space Telescope (JWST)",
@@ -62,89 +63,57 @@ export function SatelliteCardBundle({ setCardSatellite }) {
   ];
   return (
     <div className="flex space-x-4 ">
-      <SatelliteCard
-        satellite={satelliteArray[0]}
-        setCardSatellite={setCardSatellite}
-      />
-      <SatelliteCard
-        satellite={satelliteArray[1]}
-        setCardSatellite={setCardSatellite}
-      />
-      <SatelliteCard
-        satellite={satelliteArray[2]}
-        setCardSatellite={setCardSatellite}
-      />
-      <SatelliteCard
-        satellite={satelliteArray[3]}
-        setCardSatellite={setCardSatellite}
-      />
-      <SatelliteCard
-        satellite={satelliteArray[4]}
-        setCardSatellite={setCardSatellite}
-      />
+      {satelliteArray.map((satelliteName) => (
+        <SatelliteCard
+          satelliteName={satelliteName}
+          setActiveSatellite={setActiveSatellite}
+          key={satelliteName}
+        />
+      ))}
     </div>
   );
 }
 
-export function SatelliteCard({ satellite, setCardSatellite }) {
-  function handleSatelliteClick(value) {
-    setCardSatellite(value);
+function SatelliteCard({ satelliteName, setActiveSatellite }) {
+  function handleSatelliteClick() {
+    setActiveSatellite(satelliteName);
   }
   return (
     <div
       className="bg-white p-4 rounded-xl h-22 w-24"
-      onClick={() => handleSatelliteClick(satellite)}
+      onClick={handleSatelliteClick}
     >
-      <Satellite satellite={satellite} />
+      <Satellite satelliteName={satelliteName} />
     </div>
   );
 }
 
-export function Satellite({ satellite }) {
-  //   : "";
-  let satname;
-  if (satellite === "Transiting Exoplanet Survey Satellite (TESS)") {
-    satname = "tess";
-  } else if (satellite === "Transiting Exoplanet Survey Satellite (TESS)") {
-    satname = "tess";
-  } else if (satellite === "James Webb Space Telescope (JWST)") {
-    satname = "jwst";
-  } else if (satellite === "Kepler") {
-    satname = "kepler";
-  } else if (satellite === "Spitzer Space Telescope") {
-    satname = "spitzer";
-  } else if (satellite === "Hubble Space Telescope") {
-    satname = "hubble";
-  } else {
-  }
-
-  const sat = require(`./assets/${satname}.png`);
+function Satellite({ satelliteName }) {
+  const sat = require(`./assets/${satelliteName}.png`);
 
   return <img src={sat} alt="Satellite" className=" drop-shadow-2xl" />;
 }
-
-export function PlanetCard({ cardSatellite, planetTitle }) {
+function PlanetCard({ activeSatellite, planetList }) {
   const planet = require("./assets/gas-giant.png");
   return (
     <div className=" bg-slate-50  h-auto w-4/5 rounded-xl  border-solid border-4 border-sky-500 flex flex-col items-center justify-center">
-      <div className="font-mochiy">{cardSatellite || "Exoplanet Types:"}</div>
+      <div className="font-mochiy">{activeSatellite || "Exoplanet Types:"}</div>
       <div className="flex space-x-16">
-        <Planet planet={planet} planetTitle={planetTitle} />
-        <Planet planet={planet} planetTitle={planetTitle} />
-        <Planet planet={planet} planetTitle={planetTitle} />
-        <Planet planet={planet} planetTitle={planetTitle} />
-        <Planet planet={planet} planetTitle={planetTitle} />
+        {planetList.map((planetTitle) => (
+          <Planet planetTitle={planetTitle} key={planetTitle} />
+        ))}
       </div>
     </div>
   );
 }
 
-export function Planet({ planet, planetTitle }) {
+function Planet({ planetTitle }) {
+  const planet = require(`./assets/gas-giant.png`);
   return (
     <div className="flex flex-col items-center justify-center gap-y-3 size-40 mt-4 mb-4">
       <img src={planet} alt="Satellite" className=" drop-shadow-2xl h-2/3" />
       <div className=" font-kavoon text-violet-950 ">
-        {planetTitle || "Un Named Planet"}
+        {planetTitle.pl_name || "Un Named Planet"}
       </div>
     </div>
   );
